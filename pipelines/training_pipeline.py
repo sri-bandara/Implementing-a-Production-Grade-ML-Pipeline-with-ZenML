d4ca283fd372c4aa5d6287669b4f2815f9f4c8d8
Sri Bandara <chamindub69@gmail.com>
2026-01-05 18:24:27 +1100

    Add files via upload

---
+++ pipelines/training_pipeline.py
@@ -0,0 +1,14 @@
+from zenml import pipeline
+
+from steps.clean_data import clean_df
+from steps.evaluation import evaluate_model
+from steps.ingest_data import ingest_df
+from steps.model_train import train_model
+from steps.config import ModelNameConfig
+
+@pipeline(enable_cache=True)
+def train_pipeline(data_path: str):
+    df = ingest_df(data_path)
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, X_test, y_train, y_test, config=ModelNameConfig(model_name="LinearRegression"))
+    r2_score, rmse = evaluate_model(model, X_test, y_test)
