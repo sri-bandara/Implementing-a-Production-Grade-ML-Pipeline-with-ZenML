49e3f6f6d7cbd8b00ef917c85d4a9884e7b2a427
Sri Bandara <chamindub69@gmail.com>
2026-01-05 18:22:15 +1100

    Add files via upload

---
+++ run_pipeline.py
@@ -0,0 +1,4 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == '__main__':
+    train_pipeline(data_path='/Users/sribandara/Documents/mlops2/data/olist_customers_dataset.csv')
